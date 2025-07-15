b66d611434347ab9ec62c0c8502f5ffe2a1b927b
Liam <liam@fenrir.(none)>
2025-07-15 02:41:55 +0100

    feat: expand URL pattern matching with comprehensive regex support
    - Add support for srcset, CSS imports, and CSS url() patterns
    - Implement JSON-LD and lazy loading image attribute detection
    - Add relative URL pattern matching for better coverage
    - Make patterns non-greedy and multiline-aware for robust parsing
    - Extend regex identifiers for specialized web content extraction
    - Improve HTTP URL pattern to handle more edge cases and delimiters

--- internal/walker/regex.go
+++ internal/walker/regex.go
@@ -9,37 +9,63 @@ const (
 	HtmlImageRegexIdentifier
 	HtmlScriptRegexIdentifier
 	HtmlStyleRegexIdentifier
-	HtmlMetaRegexIdentifier
 	HtmlLinkRegexIdentifier
+	ImgSrcsetRegexIdentifier
+	CssImportRegexIdentifier
+	CssUrlRegexIdentifier
+	JsonUrlRegexIdentifier
+	ImgDataSrcRegexIdentifier
+	ImgLazySrcRegexIdentifier
 	EmailRegexIdentifier
 	TelRegexIdentifier
 	HttpUrlRegexIdentifier
+	RelativeUrlRegexIdentifier
 )
 
 var RegexIdentifiers = map[RegexIdentifier]*regexp.Regexp{
-	HtmlATagRegexIdentifier:   HtmlATagRegex,
-	HtmlImageRegexIdentifier:  HtmlImageRegex,
-	HtmlScriptRegexIdentifier: HtmlScriptRegex,
-	HtmlStyleRegexIdentifier:  HtmlStyleRegex,
-	HtmlMetaRegexIdentifier:   HtmlMetaRegex,
-	HtmlLinkRegexIdentifier:   HtmlLinkRegex,
-	EmailRegexIdentifier:      EmailRegex,
-	TelRegexIdentifier:        TelRegex,
-	HttpUrlRegexIdentifier:    HttpUrlRegex,
+	HtmlATagRegexIdentifier:    HtmlATagRegex,
+	HtmlImageRegexIdentifier:   HtmlImageRegex,
+	HtmlScriptRegexIdentifier:  HtmlScriptRegex,
+	HtmlStyleRegexIdentifier:   HtmlStyleRegex,
+	HtmlLinkRegexIdentifier:    HtmlLinkRegex,
+	ImgSrcsetRegexIdentifier:   ImgSrcsetRegex,
+	CssImportRegexIdentifier:   CssImportRegex,
+	CssUrlRegexIdentifier:      CssUrlRegex,
+	JsonUrlRegexIdentifier:     JsonUrlRegex,
+	ImgDataSrcRegexIdentifier:  ImgDataSrcRegex,
+	ImgLazySrcRegexIdentifier:  ImgLazySrcRegex,
+	EmailRegexIdentifier:       EmailRegex,
+	TelRegexIdentifier:         TelRegex,
+	HttpUrlRegexIdentifier:     HttpUrlRegex,
+	RelativeUrlRegexIdentifier: RelativeUrlRegex,
 }
 
 const (
 
-	// HTML link patterns for web scraping
-	htmlATagPattern   = `<a[^>]+href=["'""]([^"'""]+)["'""][^>]*>`
-	htmlImagePattern  = `<img[^>]+src=["'""]([^"'""]+)["'""][^>]*>`
-	htmlScriptPattern = `<script[^>]+src=["'""]([^"'""]+)["'""][^>]*>`
-	htmlStylePattern  = `<style[^>]+href=["'""]([^"'""]+)["'""][^>]*>`
-	htmlMetaPattern   = `<meta[^>]+content=["'""]([^"'""]+)["'""][^>]*>`
-	htmlLinkPattern   = `<link[^>]+href=["'""]([^"'""]+)["'""][^>]*>`
+	// HTML link patterns for web scraping - non-greedy for multiline support
+	htmlATagPattern   = `<a[^>]*?href=["'""]([^"'""]+?)["'""][^>]*?>`
+	htmlImagePattern  = `<img[^>]*?src=["'""]([^"'""]+?)["'""][^>]*?>`
+	htmlScriptPattern = `<script[^>]*?src=["'""]([^"'""]+?)["'""][^>]*?>`
+	htmlStylePattern  = `<style[^>]*?href=["'""]([^"'""]+?)["'""][^>]*?>`
+	htmlLinkPattern   = `<link[^>]*?href=["'""]([^"'""]+?)["'""][^>]*?>`
+	
+	// Additional patterns for comprehensive coverage - multiline aware
+	imgSrcsetPattern     = `srcset=["'""]([^"'""]+?)["'""]`
+	cssImportPattern     = `@import\s+["'""]([^"'""]+?)["'""]`
+	
+	// CSS url() patterns for background images, fonts, etc.
+	cssUrlPattern        = `url\(["']?(https?://[^\s"')\],;]+)["']?\)`
+	
+	// JSON-LD and script content URL patterns
+	jsonUrlPattern       = `["'""]([^"'""]*https?://[^\s"'""]+)["'""]`
+	
+	// Alternative image patterns for edge cases  
+	imgDataSrcPattern    = `data-src=["'""]([^"'""]+?)["'""]`
+	imgLazySrcPattern    = `<img[^>]*?data-lazy-src=["'""]([^"'""]+?)["'""][^>]*?>`
 
-	// RAW HTTP/HTTPS URLs
-	httpUrlPattern = `(https?://[^\s\)\]"']+)`
+	// RAW HTTP/HTTPS URLs - stop at HTML tags, quotes, whitespace, and brackets
+	httpUrlPattern     = `(https?://[^\s\)\]"'<>{}|\\^` + "`" + `]+)`
+	relativeUrlPattern = `href=["']([^"']+)["']`
 
 	// Email and other protocol links
 	emailPattern = `(mailto:[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})`
@@ -48,15 +74,21 @@ const (
 
 var (
 	// Compiled regex patterns for web scraping
-	HtmlATagRegex   = regexp.MustCompile(htmlATagPattern)
-	HtmlImageRegex  = regexp.MustCompile(htmlImagePattern)
-	HtmlScriptRegex = regexp.MustCompile(htmlScriptPattern)
-	HtmlStyleRegex  = regexp.MustCompile(htmlStylePattern)
-	HtmlMetaRegex   = regexp.MustCompile(htmlMetaPattern)
-	HtmlLinkRegex   = regexp.MustCompile(htmlLinkPattern)
-	EmailRegex      = regexp.MustCompile(emailPattern)
-	TelRegex        = regexp.MustCompile(telPattern)
-	HttpUrlRegex    = regexp.MustCompile(httpUrlPattern)
+	HtmlATagRegex     = regexp.MustCompile(htmlATagPattern)
+	HtmlImageRegex    = regexp.MustCompile(htmlImagePattern)
+	HtmlScriptRegex   = regexp.MustCompile(htmlScriptPattern)
+	HtmlStyleRegex    = regexp.MustCompile(htmlStylePattern)
+	HtmlLinkRegex     = regexp.MustCompile(htmlLinkPattern)
+	ImgSrcsetRegex    = regexp.MustCompile(imgSrcsetPattern)
+	CssImportRegex    = regexp.MustCompile(cssImportPattern)
+	CssUrlRegex       = regexp.MustCompile(cssUrlPattern)
+	JsonUrlRegex      = regexp.MustCompile(jsonUrlPattern)
+	ImgDataSrcRegex   = regexp.MustCompile(imgDataSrcPattern)
+	ImgLazySrcRegex   = regexp.MustCompile(imgLazySrcPattern)
+	EmailRegex        = regexp.MustCompile(emailPattern)
+	TelRegex          = regexp.MustCompile(telPattern)
+	HttpUrlRegex      = regexp.MustCompile(httpUrlPattern)
+	RelativeUrlRegex  = regexp.MustCompile(relativeUrlPattern)
 )
 
 func GetRegexes() map[RegexIdentifier]*regexp.Regexp {
